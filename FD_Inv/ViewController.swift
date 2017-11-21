d3b2dbf5b05398c61a9ff27d4a211915b4d9cdbc
miklo7 <fuat@nxtranet.com>
2017-11-21 12:10:12 -0500

    Initial Commit

---
+++ FD_Inv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FD_Inv
+//
+//  Created by Fuat on 11/21/17.
+//  Copyright © 2017 Joseph K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
